bb98bcdd43e14ffa355a6de6fa033d5357083d4f
samanthamcloughlin <62968552+smcloughlin13@users.noreply.github.com>
2020-09-09 10:40:57 -0700

    Update LZWEncoding.java

--- LZWEncoding.java
+++ LZWEncoding.java
@@ -1,1 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.*;
 
+public class LZWEncoding {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello");
+		
+		/*BufferedReader br = new BufferedReader(new FileReader("code.txt"));
+		char p = null;
+		char c = (char)br.next();
+		
+		//make the dictionary
+		//key = the string, value = the number
+		//intialize it for 0-255
+		HashMap <String, int> dictionary = new HashMap <String, int>();
+		for (int i = 0; i < 266; i++)
+		{
+			String letter = "" + (char)i;
+			dictionary.put(letter, i);
+		}
+		
+		while (br.ready())
+		{
+			
+			
+		}
+		*/
+	}
+}
